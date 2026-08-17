25f1a3b750d63a5f6427803e413520c099399714
Zhenbo Li <lizhenbo@yahoo.com>
2026-08-16 21:36:17 -0400

    Upgrade AGP version
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     // 8.10.x is the newest AGP that still runs on Gradle 8.11.1 (8.11 needs
     // Gradle 8.13) and the first line that knows about compileSdk 36.
-    id("com.android.application") version "8.13.2" apply false
-    id("com.android.library") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application") version "9.3.1" apply false
+    id("com.android.library") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
